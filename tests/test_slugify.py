192cba423715c6d76c457bf65b767ff25fd88f89
Danila271 <24301215@live.inueco.ru>
2026-04-14 18:11:33 +0500

    Add files via upload

---
+++ tests/test_slugify.py
@@ -0,0 +1,17 @@
+import unittest
+from qautils.slugify import add
+
+
+class TestAdd(unittest.TestCase):
+    def test_add_two_positive_numbers(self):
+        self.assertEqual(add(2, 3), 5)
+
+    def test_add_negative_numbers(self):
+        self.assertEqual(add(-1, -1), -2)
+
+    def test_add_zero(self):
+        self.assertEqual(add(0, 5), 5)
+
+
+if __name__ == "__main__":
+    unittest.main()
